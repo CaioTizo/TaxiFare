ff8147929f400a04d34a2fb9e4009ad79f07f967
Caio Tizo <caio.tizo@gmail.com>
2022-03-26 18:48:40 +0000

    small change. included optimization for datafames

--- TaxiFareModel/data.py
+++ TaxiFareModel/data.py
@@ -13,6 +13,28 @@ DIST_ARGS = dict(start_lat="pickup_latitude",
                  end_lat="dropoff_latitude",
                  end_lon="dropoff_longitude")
 
+def df_optimized(df, verbose=True, **kwargs):
+    """
+    Reduces size of dataframe by downcasting numeircal columns
+    :param df: input dataframe
+    :param verbose: print size reduction if set to True
+    :param kwargs:
+    :return: df optimized
+    """
+    in_size = df.memory_usage(index=True).sum()
+    # Optimized size here
+    for type in ["float", "integer"]:
+        l_cols = list(df.select_dtypes(include=type))
+        for col in l_cols:
+            df[col] = pd.to_numeric(df[col], downcast=type)
+            if type == "float":
+                df[col] = pd.to_numeric(df[col], downcast="integer")
+    out_size = df.memory_usage(index=True).sum()
+    ratio = (1 - round(out_size / in_size, 2)) * 100
+    GB = out_size / 1000000000
+    if verbose:
+        print("optimized size by {} % | {} GB".format(ratio, GB))
+    return df
 
 @simple_time_tracker
 def get_data(nrows=1000, local=False, **kwargs):
@@ -23,6 +45,7 @@ def get_data(nrows=1000, local=False, **kwargs):
     else:
         path = AWS_BUCKET_PATH
     df = pd.read_csv(path, nrows=nrows)
+    df = df_optimized(df)
     return df
 
 
